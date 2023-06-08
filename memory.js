a5d1f2059cdc1ea066decae98b825ff130853086
YilmazNL2005 <99069863@mydavinci.nl>
2023-06-08 15:47:07 +0200

    end

--- memory.js
+++ memory.js
@@ -1,7 +1,31 @@
 document.write ("Hallo");
-let plaatjes = [];
-plaatjes.push("afbeeldingen/pikachu.png");
-console.log(plaatjes);
+let plaatjes = [
+    "afbeeldingen/pikachu.png",
+    "afbeeldingen/piplup.png",
+    "afbeeldingen/mew.png",
+    "afbeeldingen/pokedex.png",
+    "afbeeldingen/pokeball.png",
+    "afbeeldingen/greninja.png",
+    "afbeeldingen/caterpie.png",
+    "afbeeldingen/bulbasaur.png",
+    "afbeeldingen/eevee.png",
+    "afbeeldingen/meowth.png"
+];
+
+// document.getElementById(images) = plaatjes;
+for (let i = 0; i < 10; i++) {
+    div1 = document.getElementById("images");
+    const img = document.createElement("img");
+    img.src = plaatjes[i];
+    document.body.appendChild(img);
+    }
+    
+
+
+
+
+
+
 
 
 // Met de afbeeldingen die ik moet gebruiken als memory kaarten,
