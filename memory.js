bda2585b0672eca4a853d80fbc7c9dd04d99961a
YilmazNL2005 <99069863@mydavinci.nl>
2023-06-08 12:16:03 +0200

    pause

--- memory.js
+++ memory.js
@@ -1,4 +1,9 @@
 document.write ("Hallo");
 let plaatjes = [];
-plaatjes.push(afbeeldingen/pikachu.png);
+plaatjes.push("afbeeldingen/pikachu.png");
 console.log(plaatjes);
+
+
+// Met de afbeeldingen die ik moet gebruiken als memory kaarten,
+// wil ik in een array / list plaatsen
+// Vervolgens wil ik steeds als de gebruiker 2 kaarten heeft geselecteerd kunnen controleren of het 2 dezelfde afbeeldingen zijn.
